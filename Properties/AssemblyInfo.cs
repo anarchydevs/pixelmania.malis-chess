3c9bbed9964cabb16ba452cd5f21683fa489d0f0
Pixelmania <abdulah.karic@outlook.com>
2022-01-27 20:44:52 +0100

    fixed naming conventions

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mali Chess")]
+[assembly: AssemblyTitle("Malis Chess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mali Chess")]
+[assembly: AssemblyProduct("Malis Chess")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
